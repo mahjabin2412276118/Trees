ed49a65b009749dbdc97b6d9347c89dfe9bd3868
mahjabin2412276118 <s2412276118@ru.ac.bd>
2025-12-02 20:02:57 +0600

    Insert and Search

--- ExtendedTree.cpp
+++ ExtendedTree.cpp
@@ -12,3 +12,26 @@ struct Node{
     }
 };
 
+Node insertBST(Node* root, int val){
+    if(root==NULL){
+        return new Node(val);
+    }
+    else if(val<root->data){
+        root->left=insertBST(root->left,val);
+    }
+    else{
+        root->right=insertBST(root->right,val);
+    }
+    return root;
+}
+
+bool search(Node* root, int key){
+    if(root==NULL)return false;
+    if(root->data==key) return true;
+
+    else if(key<root->data){
+        return search(root->left,key);
+    }
+    else return search(root->right,key);
+}
+
